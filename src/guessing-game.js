b1d7e29d52cbc0a35f712bf3e6b4e298d6ba8cbd
myspecialspace <myspecialspace@users.noreply.github.com>
2022-08-15 12:10:47 +0300

    feat: solve guessing game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
+    //use binary search algorithm for task
+    //граф кот.не имеет цикла(узлы и связи кот.не образуют цикла) = бинарный - у кажд.узла не более 2 потомков
     constructor() {}
 
+    //accepts min & max value of range of number to guess
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
+    //returns solution of candidate (you make an assumption based on range and previous assumptions)
     guess() {
-
+        //Метод Math.ceil() - округление вверх. Округляет аргумент до ближайшего большего целого.
+        this.maybe = Math.round((this.max + this.min)/2);
+        return this.maybe;
     }
 
+    //method is called if prev call of guess() returned number > than answer
     lower() {
-
+        this.max = this.maybe;
     }
 
+    //method is called if prev call of guess() returned number < than answer
     greater() {
-
+        this.min = this.maybe;
     }
 }
 
